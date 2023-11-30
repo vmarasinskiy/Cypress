e000b2d2a7b562f5b512be847184309f1b482e3d
vmarasinskiy <vmarasinskiy@gmail.com>
2023-11-30 15:40:06 +0300

    update verifyPageload_5.1.2

--- cypress/e2e/Verifier website/verifyPageLoad.cy.js
+++ cypress/e2e/Verifier website/verifyPageLoad.cy.js
@@ -1,5 +1,5 @@
 describe("Verifier Website - Тест загрузки страницы", () => {
-  it("Должен успешно загрузить страницу сайта sqlverifier", () => {
+  it("Должен успешно загрузить страницу сайта sqlverifier-live", () => {
     cy.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/");
 
     cy.get("#app-header > nav > div > a > span.brand-title").should("be.visible");
