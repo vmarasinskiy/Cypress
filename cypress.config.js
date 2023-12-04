cb9e22bb947d0f63fe61f9d9b30fc301d85cc5a7
vmarasinskiy <vmarasinskiy@gmail.com>
2023-12-04 13:11:14 +0300

    update cypress config and package file_5.2.1

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "2ct4x2",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+          // implement node event listeners here
+    }
+  }
 });
