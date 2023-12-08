6032e10325d20c7560657f3fe9abfec20d86e4e3
vmarasinskiy <vmarasinskiy@gmail.com>
2023-12-08 18:11:25 +0300

    update_5.2.4

--- test-electron.cy.js
+++ test-electron.cy.js
@@ -1,6 +1,5 @@
 describe("Verifier Website - Тест загрузки страницы в Electron", () => {
   it("Должен успешно загрузить страницу сайта sqlverifier-live", () => {
-    Cypress.config("baseUrl", "https://sqlverifier-staging-08050d656f7a.herokuapp.com");
     cy.visit('');
 
     cy.get("#app-header > nav > div > a > span.brand-title").should("be.visible");
