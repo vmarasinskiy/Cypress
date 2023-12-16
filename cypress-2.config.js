25e4d3ec769c6f4f61b9c92498ebe633d2b7d2fa
vmarasinskiy <vmarasinskiy@gmail.com>
2023-12-16 14:43:58 +0300

    update_5.3.4

--- cypress-2.config.js
+++ cypress-2.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "khktqq",
+  projectId: "nydtap",
   e2e: {
     baseUrl: 'https://sqlverifier-staging-08050d656f7a.herokuapp.com',
     watchForFileChanges: false,
--- cypress/e2e/Verifier website/first config/verifyPageLoad.cy.js
+++ cypress/e2e/Verifier website/first config/verifyPageLoad.cy.js
@@ -2,6 +2,6 @@ describe("Verifier Website - Тест загрузки страницы", () => 
   it("Должен успешно загрузить страницу сайта sqlverifier-live", () => {
     cy.visit("/");
 
-    cy.get("#app-header > nav > div > a > span.brand-title").should("be.visible");
+    cy.contains("Home").click({force: true});
   });
 });
